e35f95bd2709ea5d4027b342e309782b35d477b0
fan <632947957@qq.com>
2019-11-15 15:15:11 +0800

    update

---
+++ src/test/java/depends/extractor/java/MyJavaTest.java
@@ -0,0 +1,54 @@
+package depends.extractor.java;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import depends.deptypes.DependencyType;
+import depends.entity.Entity;
+import depends.entity.TypeEntity;
+import depends.extractor.FileParser;
+import depends.relations.Relation;
+
+public class MyJavaTest extends JavaParserTest{
+	@Before
+	public void setUp() {
+		super.init();
+	}
+	
+	@Test
+	public void test_wildcard_import_should_be_lookedup() throws IOException {
+		String[] srcs = new String[] {
+	    		"./src/test/resources/java-code-examples/MyJavaTest/test/A.java",
+	    		"./src/test/resources/java-code-examples/MyJavaTest/test/B.java"
+	    	    };
+	    
+	    for (String src:srcs) {
+		    FileParser parser = createParser(src);
+		    parser.parse();
+	    }
+	    inferer.resolveAllBindings();
+	    TypeEntity type = (TypeEntity)(entityRepo.getEntity("test.B"));
+	    List<Relation> relations = type.getRelations();
+	    for(Relation relation : relations) {
+	    	System.out.println(relation.getType() + " " + relation.getEntity());
+	    }
+	    assertTrue(assertRelation(type, DependencyType.CONTAIN, "test.A")
+	    		&& assertRelation(type, DependencyType.INHERIT, "test.A"));
+	}
+	
+	protected boolean assertRelation(Entity inEntity, String dependencyType, String dependedEntityFullName) {
+		for (Relation r:inEntity.getRelations()) {
+			if (r.getType().equals(dependencyType)) {
+				if (r.getEntity().getQualifiedName().equals(dependedEntityFullName)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
