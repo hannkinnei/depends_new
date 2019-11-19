61a01819b199e77a3ffd004641bd40f9a01d2617
fan <632947957@qq.com>
2019-11-19 19:40:54 +0800

    update

--- src/test/java/depends/extractor/java/MyJavaTest.java
+++ src/test/java/depends/extractor/java/MyJavaTest.java
@@ -32,6 +32,12 @@ public class MyJavaTest extends JavaParserTest{
 		    parser.parse();
 	    }
 	    inferer.resolveAllBindings();
+	    /*entityRepo.getEntities().forEach(entity -> {
+	    	System.out.println(entity.getClass() + " " + entity);
+	    	entity.getRelations().forEach(relation -> {
+	    		System.out.println(relation.getType() + " " + relation.getEntity().getClass() + " " + relation.getEntity());
+	    	});
+	    });*/
 	    TypeEntity type = (TypeEntity)(entityRepo.getEntity("test.B"));
 	    List<Relation> relations = type.getRelations();
 	    for(Relation relation : relations) {
